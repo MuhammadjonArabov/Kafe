8b3f8227d34a535dec20a39397dbfdb9fbe1ba3c
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-20 16:15:43 +0500

    create prooduct_api app

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="My API",
+        title="Kafe API",
         default_version='v1',
         description="Test API",
         terms_of_service="https://www.google.com/policies/terms/",
@@ -20,7 +20,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('product.urls')),
-                  path('swagger/', schema_view.as_view()),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('product.urls')),
+    #path('api/', include('product_api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
